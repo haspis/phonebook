5901ba5d280188b32aac938925247819c3f184b6
haspi <hsp_s@naver.com>
2022-09-01 12:44:54 +0900

    PhoneBook..

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, {Component} from "react";
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+  id = 3
+  state = {
+    information: [
+      {
+        name: '이수형',
+        phone: '010-1111-1111',
+        id: 1
+      },
+      {
+        name: '이지웅',
+        phone: '010-2222-2222',
+        id: 2
+      }
+    ]
+  }
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
   render() {
     return (
@@ -11,6 +30,7 @@ class App extends Component {
         <PhoneForm
           onCreate={this.handleCreate}
         />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
---
+++ src/components/PhoneInfo.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class PhoneInfo extends Component {
+    static defaultProps = {
+        info: {
+            name: '이름',
+            phone: '010-0000-0000',
+            id: 0
+        }
+    }
+
+    redenr() {
+        const style = {
+            border: '1px solid black',
+            padding: '8px',
+            margin: '8px'
+        };
+
+        const {
+            name, phone, id
+        } = this.props.info;
+
+        console.log(this.props.info);
+
+        return (
+            <div style={style}>
+                <div><b>{name}</b></div>
+                <div>{phone}</div>
+            </div>
+        );
+    }
+}
+
+export default PhoneInfo;
